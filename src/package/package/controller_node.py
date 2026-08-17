fd60dc627a8e39c8cd4257526f08ff3c1c7e8c7e
mohamed-k0 <mohamed.khaled.sh22@gmail.com>
2026-08-17 21:01:56 +0300

    sourcing the workspace and declaring parameters

---
+++ src/package/package/controller_node.py
@@ -0,0 +1,59 @@
+
+import rclpy
+from rclpy.node import Node
+
+from geometry_msgs.msg import Twist
+from turtlesim.msg import Color
+from std_msgs.msg import String
+
+
+
+class Controller(Node):
+    def __init__(self):
+        super().__init__('controller_node')
+
+        # Declare parameters to prevent hard-coding
+
+        self.declare_parameter('cmd_vel_topic', '/cmd_vel')  # declare a parameter of name cmd_vel_topic with default value /cmd_vel
+        self.declare_parameter('dominant_color_topic', '/dominant_color')
+        self.declare_parameter('color_sensor_topic', '/turtle1/color_sensor')
+
+        # Getting the parameters 
+
+        cmd_vel_topic = self.get_parameter('cmd_vel_topic').value 
+        dominant_color_topic = self.get_parameter('dominant_color_topic').value
+        color_sensor_topic = self.get_parameter('color_sensor_topic').value
+
+        # Declare Sublisher of velocity commands
+
+        self.vel_publisher = self.create_publisher(Twist, cmd_vel_topic, 10)
+
+        # Declare Subscriber to color sensor of turtle
+
+        self.color_subscriber = self.create_subscription(Color, color_sensor_topic, self.callback_color, 10)
+
+
+
+
+    def move_forward(self):
+        msg = Twist()
+        msg.linear.x = 1.0
+        self.publisher.publish(msg)
+        
+
+def main():
+
+    rclpy.init()
+
+
+
+    ...
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
