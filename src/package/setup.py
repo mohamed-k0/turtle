b092de3806337270116d0b291dfadd3ed60f8e38
mohamed-k0 <mohamed.khaled.sh22@gmail.com>
2026-08-18 09:27:15 +0300

    Adding the controller node and the standard turtle sim node to the launch descripton function

--- src/package/setup.py
+++ src/package/setup.py
@@ -11,7 +11,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=['setuptools'],
+    install_requires=['setuptools','pynput'],
     zip_safe=True,
     maintainer='mohamed-khaled',
     maintainer_email='mohamed.khaled.sh22@gmail.com',
