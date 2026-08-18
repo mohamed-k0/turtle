761b814a7fa7b3fdff03c3f3570e0db6d75e801f
mohamed-k0 <mohamed.khaled.sh22@gmail.com>
2026-08-18 09:47:26 +0300

    modifying package name

---
+++ build/package/build/lib/package/controller_node.py
@@ -0,0 +1,109 @@
+
+import rclpy
+from rclpy.node import Node
+
+from geometry_msgs.msg import Twist
+from turtlesim.msg import Color
+from std_msgs.msg import String
+
+from pynput import keyboard
+
+
+
+class Controller(Node):
+    def __init__(self):
+        super().__init__('controller_node')
+
+        # Declare parameters to prevent hard-coding
+
+        self.declare_parameter('cmd_vel_topic', '/cmd_vel')  # declare a parameter of name cmd_vel_topic with default value /cmd_vel
+        self.declare_parameter('dominant_color_topic', '/dominant_color')
+        self.declare_parameter('color_sensor_topic', '/turtle1/color_sensor')
+
+        # Getting the parameters 
+
+        cmd_vel_topic = self.get_parameter('cmd_vel_topic').value 
+        dominant_color_topic = self.get_parameter('dominant_color_topic').value
+        color_sensor_topic = self.get_parameter('color_sensor_topic').value
+
+        # Declare Publisher of velocity commands
+
+        self.vel_publisher = self.create_publisher(Twist, cmd_vel_topic, 10)
+
+        # Declare Subscriber to color sensor of turtle
+
+        self.color_subscriber = self.create_subscription(Color, color_sensor_topic, self.callback_color, 10)
+
+
+        # Declare Publisher of dominant color
+        self.dominant_publisher = self.create_publisher(String, dominant_color_topic, 10)
+
+        # Setting Speeds
+        self.linear_speed = 2.0
+        self.angular_speed = 2.0
+
+        # Start the keyboard listening
+        self.listener = keyboard.Listener(on_press=self.key_press)
+        self.listener.start()
+
+        print("Controller started. Use W-A-S-D")
+
+        # Define a method to read keyboard input and publish velocity commands
+
+    def key_press(self, key):
+        # Defining a message of type Twist
+        msg = Twist()
+        # Handle key press exceptions
+        try:
+            # Checking key pressed case insensitive
+            if key.char.lower() == 'w':
+                msg.linear.x = self.linear_speed
+            elif key.char.lower() == 's':
+                msg.linear.x = -self.linear_speed
+            elif key.char.lower() == 'a':
+                msg.angular.z = self.angular_speed
+            elif key.char.lower() == 'd':
+                msg.angular.z = -self.angular_speed
+            else:
+                return  # Ignore other keys
+
+            # Publish the velocity command
+            self.vel_publisher.publish(msg)
+        # Neglecting undefined key presses
+        except AttributeError: 
+            pass
+
+    def callback_color(self, msg):
+
+        # Initialize a String message that holds the dominant color
+        color_msg = String()
+
+        if msg.r > msg.g and msg.r > msg.b:
+            dominant_color = "Red"
+        elif msg.g > msg.r and msg.g > msg.b:
+            dominant_color = "Green"
+        elif msg.b > msg.r and msg.b > msg.g:
+            dominant_color = "Blue"
+        else:
+            dominant_color = "Equal"
+
+        # get the value of dominant color and store it in String message
+        color_msg.data = dominant_color
+        # Publish the dominant color message
+        self.dominant_publisher.publish(color_msg)
+
+    
+
+def main():
+
+    rclpy.init()
+    node = Controller()
+
+    rclpy.spin(node)
+
+    node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
--- src/package/setup.py
+++ src/package/setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-package_name = 'controller'
+package_name = 'package'
 
 setup(
     name=package_name,
@@ -10,6 +10,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', ['launch/controller.launch.py']),
     ],
     install_requires=['setuptools','pynput'],
     zip_safe=True,
